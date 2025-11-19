38069539a9d223e5c34540bd359b0fd0a910c715
golagaurav <ggolaibm@gmail.com>
2025-11-19 13:24:45 +0530

    fix typo

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,11 +1,9 @@
-// src/app/music/page.tsx
 import React from 'react';
 import VideoGrid from '../../components/VideoGrid';
 import { fetchVideos } from '../../lib/youtubeApi';
 
 const MusicPage = async () => {
-  const videos = await fetchVideos('music'); // Fetch music videos
-  
+  const videos = await fetchVideos('music');
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Music</h1>
